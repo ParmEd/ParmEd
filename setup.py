058bdfa84b5e79592432e4535d67fede21ce6bfe
Jason Swails <jason.swails@gmail.com>
2014-03-01 00:12:47 -0500

    Improve README formatting and fix setup.py

--- setup.py
+++ setup.py
@@ -19,9 +19,10 @@ if __name__ == '__main__':
    setup(name='ParmEd',
          version='13.5', # Corresponds to Amber release; between Amber 13 and 14
          description='Amber parameter file editor',
-         author='Jason Swails'
+         author='Jason Swails',
          author_email='jason.swails -at- gmail.com',
          url='http://jswails.wikidot.com/parmed',
+         license='GPL v2 or later',
          packages=packages,
          py_modules=modules,
          scripts=scripts)
