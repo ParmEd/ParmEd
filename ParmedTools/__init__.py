ec3d58cf0304de637916694e644053aed62cae0a
Jason Swails <jason.swails@gmail.com>
2014-03-09 17:08:55 -0400

    Significant improvement to the ParmEd API. Now you can import actions via the command:
    from ParmedTools import HMassRepartition, change, setBond, deleteBond
    
    or
    
    from ParmedTools import *
    
    which imports names that match the names in the manual and that are
    autocompleted in the interactive interpreter. The past behavior required users
    to import all-lowercase action names from ParmedTools.ParmedActions (which is
    where and how they are implemented). It was implemented this way originally to
    allow the ParmEd interpreter to be case-insensitive, but could be confusing.
    
    The old way still works fine.

--- ParmedTools/__init__.py
+++ ParmedTools/__init__.py
@@ -2,8 +2,20 @@
 
 __version__ = '14.0'
 __author__ = 'Jason Swails'
+__all__ = [] # This is populated with the ParmEd Actions below
 
 # For the purposes of the API, all of the actions from ParmedActions will be
 # imported here and renamed according to the camelCase convention used in
 # ParmedActions.Usages. Importing directly from ParmedActions will cease to be
 # documented. This should clarify things.
+
+import ParmedTools.ParmedActions as _PA
+
+for key in _PA.Usages:
+   # Skip actions that only make sense for the ParmEd interpreter
+   if key in ('help', 'go', 'quit', 'source', 'ls'): continue
+   name = _PA.Usages[key].split()[0]
+   exec('%s = _PA.%s' % (name, key))
+   __all__.append(name)
+
+del _PA
