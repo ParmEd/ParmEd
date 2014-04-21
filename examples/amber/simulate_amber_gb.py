d4d1d25c1c5812a0c86e2df60f34501ef749e081
Jason Swails <jason.swails@gmail.com>
2014-04-21 14:27:05 -0400

    Add 2 examples using Amber files.

---
+++ examples/amber/simulate_amber_gb.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+from __future__ import division, print_function
+
+import sys
+
+# OpenMM Imports
+import simtk.unit as u
+import simtk.openmm as mm
+import simtk.openmm.app as app
+
+# ParmEd Imports
+from chemistry.amber.openmmloader import OpenMMAmberParm as AmberParm
+from chemistry.amber.openmmreporters import (
+            AmberStateDataReporter, NetCDFReporter)
+
+# Load the CHARMM files
+print('Loading Amber files...')
+ala5_gas = AmberParm('ala5_gas.parm7', 'ala5_gas.rst7')
+
+# Create the OpenMM system
+print('Creating OpenMM System')
+system = ala5_gas.createSystem(nonbondedMethod=app.NoCutoff,
+                               constraints=app.HBonds, implicitSolvent=app.GBn2,
+                               implicitSolventSaltConc=0.1*u.moles/u.liter,
+)
+
+# Create the integrator to do Langevin dynamics
+integrator = mm.LangevinIntegrator(
+                        300*u.kelvin,       # Temperature of heat bath
+                        1.0/u.picoseconds,  # Friction coefficient
+                        2.0*u.femtoseconds, # Time step
+)
+
+# Define the platform to use; CUDA, OpenCL, CPU, or Reference. Or do not specify
+# the platform to use the default (fastest) platform
+platform = mm.Platform.getPlatformByName('CUDA')
+prop = dict(CudaPrecision='mixed') # Use mixed single/double precision
+
+# Create the Simulation object
+sim = app.Simulation(ala5_gas.topology, system, integrator, platform, prop)
+
+# Set the particle positions
+sim.context.setPositions(ala5_gas.positions)
+
+# Minimize the energy
+print('Minimizing energy')
+sim.minimizeEnergy(maxIterations=500)
+
+# Set up the reporters to report energies and coordinates every 100 steps
+sim.reporters.append(
+        AmberStateDataReporter(sys.stdout, 100, step=True, potentialEnergy=True,
+                               kineticEnergy=True, temperature=True,
+                               volume=True, density=True)
+)
+sim.reporters.append(
+        NetCDFReporter('ala5_gb.nc', 100, atom=ala5_gas.ptr('natom'),
+                       uses_pbc=False, crds=True)
+)
+
+# Run dynamics
+print('Running dynamics')
+sim.step(10000)
---
+++ examples/amber/simulate_amber_pme.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+from __future__ import division, print_function
+
+import sys
+
+# OpenMM Imports
+import simtk.unit as u
+import simtk.openmm as mm
+import simtk.openmm.app as app
+
+# ParmEd Imports
+from chemistry.amber.openmmloader import OpenMMAmberParm as AmberParm
+from chemistry.charmm.parameters import CharmmParameterSet
+from chemistry.amber.openmmreporters import (AmberStateDataReporter,
+                                             NetCDFReporter)
+
+# Load the CHARMM files
+print('Loading AMBER files...')
+ala2_solv = AmberParm('ala2_solv.parm7', 'ala2_solv.rst7')
+
+# Create the OpenMM system
+print('Creating OpenMM System')
+system = ala2_solv.createSystem(nonbondedMethod=app.PME,
+                                nonbondedCutoff=8.0*u.angstroms,
+                                constraints=app.HBonds,
+)
+
+# Create the integrator to do Langevin dynamics
+integrator = mm.LangevinIntegrator(
+                        300*u.kelvin,       # Temperature of heat bath
+                        1.0/u.picoseconds,  # Friction coefficient
+                        2.0*u.femtoseconds, # Time step
+)
+
+# Define the platform to use; CUDA, OpenCL, CPU, or Reference. Or do not specify
+# the platform to use the default (fastest) platform
+platform = mm.Platform.getPlatformByName('CUDA')
+prop = dict(CudaPrecision='mixed') # Use mixed single/double precision
+
+# Create the Simulation object
+sim = app.Simulation(ala2_solv.topology, system, integrator, platform, prop)
+
+# Set the particle positions
+sim.context.setPositions(ala2_solv.positions)
+
+# Minimize the energy
+print('Minimizing energy')
+sim.minimizeEnergy(maxIterations=500)
+
+# Set up the reporters to report energies and coordinates every 100 steps
+sim.reporters.append(
+        AmberStateDataReporter(sys.stdout, 100, step=True, potentialEnergy=True,
+                              kineticEnergy=True, temperature=True,
+                              volume=True, density=True)
+)
+sim.reporters.append(
+        NetCDFReporter('ala2_solv.nc', 100, atom=ala2_solv.ptr('natom'),
+                       uses_pbc=True, crds=True)
+)
+
+# Run dynamics
+print('Running dynamics')
+sim.step(10000)
