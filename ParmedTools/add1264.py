00f68b94eeba859050b8b7be1bb37775e6534c4f
Jason Swails <jason.swails@gmail.com>
2015-03-11 09:17:23 -0400

    Changes from Pengfei
    Fixes an issue in add12_6_4 that Pengfei fixed awhile ago, but I inadvertently
    reversed (not sure how that happened). Also updates add12_6_4 to add parameters
    for monovalent, trivalent, and tetravalent ions.

--- ParmedTools/add1264.py
+++ ParmedTools/add1264.py
@@ -9,20 +9,41 @@ import warnings
 
 WATER_POL = 1.444 # Polarizability of water
 
-DEFAULT_C4_PARAMS = dict(
-        TIP3P = dict(Be=186.5, Cu=290.9, Ni=212.8, Zn=231.6, Co=209.7,
-                     Cr=136.8, Fe=163.0, Mg=132.9, V=195.7, Mn=146.1,
-                     Hg=288.8, Cd=185.6, Ca=87.3, Sn=187.9, Sr=82.7,
-                     Ba=71.9),
-        TIP4PEW = dict(Be=228.5, Cu=339.2, Ni=259.2, Zn=272.3, Co=252.8,
-                       Cr=177.4, Fe=201.1, Mg=180.5, V=244.8, Mn=192.3,
-                       Hg=335.2, Cd=233.7, Ca=128.0, Sn=231.4, Sr=118.9,
-                       Ba=112.5),
-        SPCE = dict(Be=188.1, Cu=304.4, Ni=205.2, Zn=231.2, Co=209.2,
-                    Cr=131.2, Fe=155.4, Mg=122.2, V=206.6, Mn=154.9,
-                    Hg=300.2, Cd=198.8, Ca=89.0, Sn=201.1, Sr=96.3,
-                    Ba=85.8),
-)
+DEFAULT_C4_PARAMS = {
+        'TIP3P' : {'Li1': 27.0, 'Na1': 0.0, 'K1': 8.0, 'Rb1': 0.0, 'Cs1': 2.0, 'Tl1': 50.0,
+                   'Cu1': 7.0, 'Ag1': 83.0, 'F-1': -27.0, 'Cl-1': -38.0, 'Br-1': -39.0,
+                   'I-1': -45.0, 'Be2': 186.5, 'Cu2': 290.9, 'Ni2': 212.8, 'Zn2': 231.6,
+                   'Co2': 209.7, 'Cr2': 136.8, 'Fe2': 163.0, 'Mg2': 132.9, 'V2': 195.7,
+                   'Mn2': 146.1, 'Hg2': 288.8, 'Cd2': 185.6, 'Ca2': 87.3, 'Sn2': 187.9,
+                   'Sr2': 82.7, 'Ba2': 71.9, 'Al3': 399.0, 'Fe3': 428.0, 'Cr3': 258.0,
+                   'In3': 347.0, 'Tl3': 456.0, 'Y3': 216.0, 'La3': 152.0, 'Ce3': 230.0,
+                   'Pr3': 264.0, 'Nd3': 213.0, 'Sm3': 230.0, 'Eu3': 259.0, 'Gd3': 198.0,
+                   'Tb3': 235.0, 'Dy3': 207.0, 'Er3': 251.0, 'Tm3': 282.0, 'Lu3': 249.0,
+                   'Hf4': 827.0, 'Zr4': 761.0, 'Ce4': 706.0, 'U4': 1034.0, 'Pu4': 828.0,
+                   'Th4': 512.0},
+        'TIP4PEW' : {'Li1': 36.0, 'Na1': 9.0, 'K1': 24.0, 'Rb1': 13.0, 'Cs1': 16.0, 'Tl1': 65.0,
+                     'Cu1': 21.0, 'Ag1': 94.0, 'F-1': -67.0, 'Cl-1': -66.0, 'Br-1': -68.0,
+                     'I-1': -62.0, 'Be2': 228.5, 'Cu2': 339.2, 'Ni2': 259.2, 'Zn2': 272.3,
+                     'Co2': 252.8, 'Cr2': 177.4, 'Fe2': 201.1, 'Mg2': 180.5, 'V2': 244.8,
+                     'Mn2': 192.3, 'Hg2': 335.2, 'Cd2': 233.7, 'Ca2': 128.0, 'Sn2': 231.4,
+                     'Sr2': 118.9, 'Ba2': 112.5, 'Al3': 488.0, 'Fe3': 519.0, 'Cr3': 322.0,
+                     'In3': 425.0, 'Tl3': 535.0, 'Y3': 294.0, 'La3': 243.0, 'Ce3': 315.0,
+                     'Pr3': 348.0, 'Nd3': 297.0, 'Sm3': 314.0, 'Eu3': 345.0, 'Gd3': 280.0,
+                     'Tb3': 313.0, 'Dy3': 209.0, 'Er3': 328.0, 'Tm3': 356.0, 'Lu3': 331.0,
+                     'Hf4': 956.0, 'Zr4': 895.0, 'Ce4': 835.0, 'U4': 1183.0, 'Pu4': 972.0,
+                     'Th4': 625.0},
+        'SPCE' :  {'Li1': 33.0, 'Na1': 6.0, 'K1': 19.0, 'Rb1': 7.0, 'Cs1': 12.0, 'Tl': 61.0,
+                  'Cu1': 9.0, 'Ag1': 92.0, 'F-1': -53.0, 'Cl-1': -55.0, 'Br-1': -51.0,
+                  'I-1': -51.0, 'Be2': 188.1, 'Cu2': 304.4, 'Ni2': 205.2, 'Zn2': 231.2,
+                  'Co2': 209.2, 'Cr2': 131.2, 'Fe2': 155.4, 'Mg2': 122.2, 'V2': 206.6,
+                  'Mn2': 154.9, 'Hg2': 300.2, 'Cd2': 198.8, 'Ca2': 89.0, 'Sn2': 201.1,
+                  'Sr2': 96.3, 'Ba2': 85.8, 'Al3': 406.0, 'Fe3': 442.0, 'Cr3': 254.0,
+                  'In3': 349.0, 'Tl3': 455.0, 'Y3': 209.0, 'La3': 165.0, 'Ce3': 242.0,
+                  'Pr3': 272.0, 'Nd3': 235.0, 'Sm3': 224.0, 'Eu3': 273.0, 'Gd3': 186.0,
+                  'Tb3': 227.0, 'Dy3': 206.0, 'Er3': 247.0, 'Tm3': 262.0, 'Lu3': 247.0,
+                  'Hf4': 810.0, 'Zr4': 760.0, 'Ce4': 694.0, 'U4': 1043.0, 'Pu4': 828.0,
+                  'Th4': 513.0}
+}
 
 def params1264(parm, mask, c4file, watermodel, polfile, tunfactor):
    
@@ -53,8 +74,9 @@ def params1264(parm, mask, c4file, watermodel, polfile, tunfactor):
     typelist = dict()
     for i in mask.Selected():
         typeidx = parm.parm_data['ATOM_TYPE_INDEX'][i]
+        chgi = parm.parm_data['CHARGE'][i]
         if typeidx in typelist: continue
-        typelist[typeidx] = parm.atoms[i].atomic_number
+        typelist[typeidx] = (parm.atoms[i].atomic_number, int(chgi))
         print "The selected metal ion is", \
                 pt.Element[parm.atoms[i].atomic_number]
     types = typelist.keys()
@@ -97,7 +119,7 @@ def params1264(parm, mask, c4file, watermodel, polfile, tunfactor):
             jj = atomtypeindexlist2[j] - 1
             idx = parm.parm_data['NONBONDED_PARM_INDEX'][ntypes*jj+i]-1
             try:
-                c4 = c4list[ pt.Element[typelist[typ]] ]
+                c4 = c4list[ pt.Element[typelist[typ][0]] + str(typelist[typ][1]) ]
                 pol = pollist[atomtypej]
 #               print ('ATOM_TYPE_INDEX = %d; AMBER_ATOM_TYPE=%s; '
 #                      'Polarizability=%s' %(jj+1, atomtypej, pol))
@@ -116,7 +138,7 @@ def params1264(parm, mask, c4file, watermodel, polfile, tunfactor):
             jj = atomtypeindexlist2[j] - 1
             idx = parm.parm_data['NONBONDED_PARM_INDEX'][ntypes*i+jj] - 1
             try:
-                c4 = c4list[ pt.Element[typelist[typ]] ]
+                c4 = c4list[ pt.Element[typelist[typ][0]] + str(typelist[typ][1]) ]
                 pol = pollist[atomtypej]
 #               print ('ATOM_TYPE_INDEX = %d; AMBER_ATOM_TYPE= %s; '
 #                      'Polarizability=%s' %(jj+1, atomtypej, pol))
