2f9fb8c4a266db496222662e7f59e36a0e120696
Jason Swails <jason.swails@gmail.com>
2015-04-06 14:36:52 -0400

    Try to bring ParmEd's Travis-CI more in line with what mdtraj does
    Particularly with respect to conda. Now that I've used conda a bit more, I
    (think I) understand a bit better what to do. Still a bit different, since the
    test suite is not a submodule that can be executed directly via nose like mdtraj
    is.

---
+++ devtools/travis-ci/install.sh
@@ -0,0 +1,31 @@
+sudo apt-get update
+sudo apt-get install -qq -y g++ gfortran valgrind csh
+
+MINICONDA=Miniconda-latest-Linux-x86_64.sh
+MINICONDA_MD5=$(curl -s http://repo.continuum.io/miniconda/ | grep -A3 $MINICONDA | sed -n '4p' | sed -n 's/ *<td>\(.*\)<\/td> */\1/p')
+wget http://repo.continuum.io/miniconda/$MINICONDA
+if [[ $MINICONDA_MD5 != $(md5sum $MINICONDA | cut -d ' ' -f 1) ]]; then
+    echo "Miniconda MD5 mismatch"
+    exit 1
+fi
+bash $MINICONDA -b
+
+
+export PATH=$HOME/miniconda/bin:$PATH
+conda install --yes conda-build jinja2 binstar pip
+conda config --add channels http://conda.binstar.org/omnia
+
+# Python 2.6 does not have argparse, but parmed.py uses it
+if [ "$PYTHON_VERSION" = "2.6" ]; then
+    argparse="argparse"
+fi
+
+if [ -z "$NO_NUMPY" ]; then
+    conda create -y -n myenv python=$PYTHON_VERSION $argparse \
+        numpy scipy netcdf4 pandas nose openmm
+else
+    # Do not install the full numpy/scipy stack
+    conda create -y -n myenv python=$PYTHON_VERSION $argparse
+fi
+
+source activate myenv
