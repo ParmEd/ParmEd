12cadee9cdbb1467249afe29439b579d0e651fd7
Brian Radak <brian.radak.accts@gmail.com>
2015-04-15 13:45:00 -0500

    _very_ rudimentary test case for reading namdbin file

---
+++ test/test_chemistry_namd.py
@@ -0,0 +1,18 @@
+"""
+Tests the chemistry/namd module
+"""
+import unittest
+import os
+
+from utils import get_fn, has_numpy
+import chemistry.namd as namd
+
+class TestNamdBin(unittest.TestCase):
+    """Test the NamdBinCoor class."""
+    def testRead(self):
+        coor = namd.NamdBinCoor.read(get_fn('ala_ala_ala.coor'))
+        self.assertEqual(coor.natom,33)
+
+
+if __name__ == '__main__':
+    unittest.main()
