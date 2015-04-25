3bd42694ed4e6e3ee33c30553831eacdd9c43c17
Carlos Hernandez <cxh@stanford.edu>
2015-04-25 16:16:30 -0700

    split tests

--- test/test_chemistry_rosetta.py
+++ test/test_chemistry_rosetta.py
@@ -1,7 +1,7 @@
 from chemistry import read_PDB, load_rosetta
 from utils import get_fn
 import unittest
-from unittest import skipIf
+from unittest import skipIf, skipTest
 
 try:
     from simtk.openmm.app import PDBFile
@@ -19,26 +19,37 @@ def _unpackLen(obj):
     return len(list(obj))
 
 
+@skipTest(not init, "Cannot test load_rosetta module without PyRosetta.")
 class TestRosetta(unittest.TestCase):
     """ Tests loading of a Rosetta pose object """
 
-    @skipIf(not init, "Cannot test load_rosetta module without PyRosetta.")
-    def testLoadPose(self):
-        if not init or not pose_from_sequence:
-            raise ImportError('Could not load the PyRosetta module.')
+    def testLoadStruct(self):
+        """ Test load_rosetta against read_PDB"""
 
         init()
         pose = pose_from_sequence(3*'A')
+
         struct = load_rosetta(pose)
-        if PDBFile:
-            pdb = PDBFile(get_fn('ala_ala_ala.pdb'))
-            self.assertEqual(_unpackLen(struct.topology.bonds()),
-                             _unpackLen(pdb.topology.bonds()))
-        else:
-            pdb = read_PDB(get_fn('ala_ala_ala.pdb'))
+        pdb = read_PDB(get_fn('ala_ala_ala.pdb'))
+
+        self.assertEqual(len(struct.atoms), len(pdb.atoms))
+        self.assertEqual(len(struct.residues), len(pdb.residues))
+
+    @skipIf(not PDBFile, "Cannot compare topologies without OpenMM.")
+    def testLoadedTopology(self):
+        """ Test load_rosetta against OpenMM topology"""
+
+        init()
+        pose = pose_from_sequence(3*'A')
+
+        struct = load_rosetta(pose)
+        pdb = PDBFile(get_fn('ala_ala_ala.pdb'))
 
         self.assertEqual(_unpackLen(struct.topology.atoms()),
                          _unpackLen(pdb.topology.atoms()))
+
+        self.assertEqual(_unpackLen(struct.topology.bonds()),
+                         _unpackLen(pdb.topology.bonds()))
 
         self.assertEqual(_unpackLen(struct.topology.residues()),
                          _unpackLen(pdb.topology.residues()))
