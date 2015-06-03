7dcd5a348742ffa4a78fc03de17f769d14d910fc
Jason Swails <jason.swails@gmail.com>
2015-06-02 22:06:42 -0400

    Ack.  Update version numbers.  I need a better way of doing this.

--- ParmedTools/__init__.py
+++ ParmedTools/__init__.py
@@ -1,6 +1,6 @@
 """ A list of all of the tools used by parmed """
 
-__version__ = '15.1'
+__version__ = '2.0'
 __author__ = 'Jason Swails'
 __all__ = [] # This is populated with the ParmEd Actions below
 
