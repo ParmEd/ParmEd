67467f57336070127bb704f27fa97b13f8c5dcde
Jason Swails <jason.swails@gmail.com>
2015-06-08 08:04:36 -0400

    Add workaround for Mac case-insensitivity.

--- parmed.py
+++ parmed.py
@@ -4,12 +4,16 @@ import sys
 
 # Since this script is called "parmed.py" and we want to import the "parmed"
 # package, we need to remove the script's directory from sys.path to prevent the
-# script from trying to import itself.
+# script from trying to import itself. Bear in mind Mac filesystems are
+# typically case-insensitive
 scriptdir = os.path.realpath(os.path.abspath(os.path.split(__file__)[0]))
+is_mac = 'darwin' in sys.platform.lower()
 while True:
     for i, folder in enumerate(sys.path):
         folder = os.path.realpath(os.path.abspath(folder))
         if folder == scriptdir:
+            break
+        elif is_mac and folder.lower() == scriptdir.lower():
             break
     else:
         break
