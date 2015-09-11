b7845a36bb91ea132b492a8fa08ae05115e1358e
Brian Radak <brian.radak.accts@gmail.com>
2015-09-11 13:31:02 -0500

    preparing new tests, waiting to pull Jason's edits

--- test/test_parmed_namd.py
+++ test/test_parmed_namd.py
@@ -13,6 +13,12 @@ class TestNamdBin(unittest.TestCase):
         coor = namd.NamdBinCoor.read(get_fn('ala_ala_ala.coor'))
         self.assertEqual(coor.natom,33)
 
+class TestNamdVel(unittest.TestCase):
+     """Test the NamdBinVel class."""
+     def testRead(self):
+        vel = namd.NamdBinVel.read(get_fn('ala_ala_ala.vel'))
+        self.assertEqual(vel.natom,33)
+        vel.write('test.vel')
 
 if __name__ == '__main__':
     unittest.main()
