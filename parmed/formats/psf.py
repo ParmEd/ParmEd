3eb5f3b849c5e64e74f4711e20c8fc9e0f6a37f7
Jason Swails <jason.swails@gmail.com>
2015-12-16 11:42:31 -0500

    Order impropers the way CHARMM expects them to be ordered in CHARMM PSF/PAR files.

--- parmed/formats/psf.py
+++ parmed/formats/psf.py
@@ -6,6 +6,7 @@ from parmed.charmm import CharmmPsfFile
 # TODO -- move this functionality to a more centralized location
 from parmed.charmm.psf import set_molecules
 from parmed.formats.registry import FileFormatType
+from parmed.utils import canonical_improper_order
 from parmed.utils.io import genopen
 from parmed.utils.six import add_metaclass, string_types
 from parmed.utils.six.moves import range
@@ -195,14 +196,16 @@ class PSFFile(object):
         dest.write(intfmt % (nimprop) + ' !NIMPHI: impropers\n')
         def improp_gen(struct):
             for imp in struct.impropers:
-                yield imp
+                yield (imp.atom1, imp.atom2, imp.atom3, imp.atom4)
             for dih in struct.dihedrals:
                 if dih.improper:
-                    yield dih
-        for i, imp in enumerate(improp_gen(struct)):
-            dest.write((intfmt*4) % (imp.atom1.idx+1, imp.atom2.idx+1,
-                                     imp.atom3.idx+1, imp.atom4.idx+1)
-            )
+                    # Central atom first
+                    yield canonical_improper_order(
+                            dih.atom3, dih.atom1, dih.atom2,
+                            dih.atom4, center=1,
+                    )
+        for i, (a1, a2, a3, a4) in enumerate(improp_gen(struct)):
+            dest.write((intfmt*4) % (a1.idx+1, a2.idx+1, a3.idx+1, a4.idx+1))
             if i % 2 == 1: # Write 2 dihedrals per line
                 dest.write('\n')
         # See if we need to terminate
