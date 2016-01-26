5c9bf24bd37dd4127a669b3f27f7d320ba376cf4
Jason Swails <jason.swails@gmail.com>
2016-01-25 21:49:00 -0500

    Move .coveragerc and install a specific numpypy branch compatible with the latest pypy release

--- devtools/travis-ci/install.sh
+++ devtools/travis-ci/install.sh
@@ -9,7 +9,7 @@ if [ "$PYTHON_VERSION" = "pypy" ]; then
 
     pypy -m pip install nose pyflakes nose-timer
     which pyflakes
-    pypy -m pip install --user git+https://bitbucket.org/pypy/numpy.git
+    pypy -m pip install --user git+https://bitbucket.org/pypy/numpy.git@pypy-4.0.1
 else # Otherwise, CPython... go through conda
     if [ "$TRAVIS_OS_NAME" = "osx" ]; then
         wget http://repo.continuum.io/miniconda/Miniconda-3.7.0-MacOSX-x86_64.sh -O miniconda.sh;
