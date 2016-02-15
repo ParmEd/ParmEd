a046c90359e477f4aaccf8c95a2a37e3e71c1988
Jason Swails <jason.swails@gmail.com>
2016-02-14 20:40:57 -0500

    Fix __repr__ for atoms in ResidueTemplate

--- parmed/topologyobjects.py
+++ parmed/topologyobjects.py
@@ -921,8 +921,10 @@ class Atom(_ListItem):
 
     def __repr__(self):
         start = '<Atom %s [%d]' % (self.name, self.idx)
-        if self.residue is not None:
+        if self.residue is not None and hasattr(self.residue, 'idx'):
             return start + '; In %s %d>' % (self.residue.name, self.residue.idx)
+        elif self.residue is not None:
+            return start + '; In object %r>' % self.residue
         return start + '>'
 
     #===================================================
