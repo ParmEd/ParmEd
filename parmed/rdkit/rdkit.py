3f4f756daaac22dafe344f2bac17f47af8f69755
Hai Nguyen <hainm.comp@gmail.com>
2016-03-07 18:36:52 -0500

    add load_rdkit

---
+++ parmed/rdkit/rdkit.py
@@ -0,0 +1,41 @@
+"""
+This package contains classes responsible for loading rdkit objects
+"""
+
+from __future__ import print_function
+from parmed.formats import Mol2File
+
+try:
+    from rdkit import Chem
+except ImportError:
+    pass
+
+try:
+    # py2
+    import StringIO
+except ImportError:
+    import io
+    StringIO = io.StringIO
+
+class RDKit(object):
+
+    @staticmethod
+    def load(rmol):
+        """
+        Load a :class:`Mol` object and return a populated :class:`Structure`
+        instance
+
+        Parameters
+        ----------
+        rmol: :class:`Mol`
+            RDKit :class:`Mol` object to convert
+
+        Examples
+        --------
+        >>> from rdkit import Chem
+        >>> import parmed as pmd
+        >>> mol = Chem.MolFromSmiles('Cc1ccccc1')
+        >>> struct = pmd.load_rdkit(mol)
+        """
+        fh = StringIO(Chem.MolToMolBlock(rmol)
+        return Mol2File.parse(fh)
