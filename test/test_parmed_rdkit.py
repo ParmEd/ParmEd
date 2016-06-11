cafcbf67885719ea55449f3a078e131066353c63
Hai Nguyen <hainm.comp@gmail.com>
2016-06-10 22:53:27 -0400

    fix typos

--- test/test_parmed_rdkit.py
+++ test/test_parmed_rdkit.py
@@ -12,7 +12,7 @@ except ImportError:
 
 is_linux = sys.platform.startswith('linux')
 
-@unittest.skipUnless(has_rdkit and is_linux, "Onlye test load_rdkit module on Linux")
+@unittest.skipUnless(has_rdkit and is_linux, "Only test load_rdkit module on Linux")
 class TestRDKit(unittest.TestCase):
     """ Tests loading of an rdkit Mol object """
 
