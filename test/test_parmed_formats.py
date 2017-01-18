4f500576703df9c1187370f12787b4f7b1efff0e
Hai Nguyen <hainm.comp@gmail.com>
2017-01-17 22:30:43 -0500

    round trip pdb--> cif --> pdb

--- test/test_parmed_formats.py
+++ test/test_parmed_formats.py
@@ -1585,6 +1585,14 @@ class TestCIFStructure(FileIOTestCase):
         """ Tests that symmetry is parsed from mmCIF files correctly """
         self.assertEqual(download_CIF('1aki').space_group, 'P 21 21 21')
 
+    def test_cif_space_group_written_from_structure(self):
+        parm = pmd.load_file(get_fn('SCM_A.pdb'))
+        self.assertEqual(parm.space_group, 'P 1 21 1')
+        written = get_fn('test.cif', written=True)
+        parm.write_cif(written)
+        parm2 = pmd.load_file(written)
+        self.assertEqual(parm2.space_group, 'P 1 21 1')
+
     def test_cif_models(self):
         """ Test CIF parsing/writing NMR structure with 20 models (2koc) """
         cif = download_CIF('2koc')
