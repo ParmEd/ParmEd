a9886bc240e1332633a9892c3812a6624494ab18
Jason Swails <jason.swails@gmail.com>
2017-02-19 09:00:01 -0500

    Print out the conda version and pin ngl to 0.5.1

--- devtools/ci/jenkins/install.sh
+++ devtools/ci/jenkins/install.sh
@@ -14,13 +14,16 @@ conda create -yn ${CONDAENV} --no-default-packages python=${PYTHON_VERSION}
 conda config --add channels omnia
 conda config --add channels ambermd
 
+# Show the conda version
+conda --version
+
 # Now add the packages we want
 conda install -yn ${CONDAENV} numpy scipy pandas nose openmm coverage nose-timer \
                               python-coveralls ambermini=16.16 netCDF4
 conda install -yn ${CONDAENV} pyflakes=1.0.0
 conda install -yn ${CONDAENV} rdkit==2015.09.1 -c omnia
 conda install -yn ${CONDAENV} boost==1.59.0 -c omnia
-conda install -yn ${CONDAENV} nglview -c bioconda
+conda install -yn ${CONDAENV} nglview==0.5.1
 
 # Add PyRosetta4 to the PYTHONPATH so it will be available
 export PYTHONPATH=/usr/local/pyrosetta4/lib/python${PYTHON_VERSION}/site-packages
