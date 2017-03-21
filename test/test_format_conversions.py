1af7ddc9fa55a29fc6c2257ddfc47a6308ad8adc
Paul J. van Maaren <paul@vanmaaren.org>
2017-03-21 05:51:47 +0000

    Updated test_unconvertable_rb_torsion()

--- test/test_format_conversions.py
+++ test/test_format_conversions.py
@@ -323,10 +323,29 @@ class TestGromacsToAmber(FileIOTestCase, EnergyTestCase):
         # ordering of the bondingtypes and one dihedral is symmetric
         assert len(top.parameterset.rb_torsion_types) == 7
 
-        self.assertRaises(ValueError, lambda:
-                amber.AmberParm.from_structure(top)
-        )
+        parm = amber.AmberParm.from_structure(top)
+        parm.save(get_fn('rb_torsions.prmtop', written=True))
+        parm.save(get_fn('rb_torsions.rst7', written=True))
 
+        sysg = top.createSystem()
+        sysa = parm.createSystem()
+
+        cong = mm.Context(sysg, mm.VerletIntegrator(0.001), CPU)
+        cona = mm.Context(sysa, mm.VerletIntegrator(0.001), CPU)
+
+        cong.setPositions(top.positions)
+        cona.setPositions(top.positions)
+
+        self.check_energies(top, cong, parm, cona)
+	ene1 = openmm.utils.energy_decomposition(top, cong)
+	ene2 = openmm.utils.energy_decomposition(parm, cona)
+
+	self.assertAlmostEqual(ene1['total'], ene2['total'])
+	self.assertEqual(ene1['bond'], ene2['bond'])
+	self.assertEqual(ene1['angle'], ene2['angle'])
+	self.assertEqual(ene1['dihedral'], ene2['dihedral'])
+	self.assertEqual(ene1['nonbonded'], ene2['nonbonded'])
+	self.assertEqual(ene1['total'], ene2['total'])
 
     @unittest.skipUnless(HAS_OPENMM, "Cannot test without OpenMM")
     def test_energy_complicated(self):
