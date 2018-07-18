0734f2a9164a23edb6861cec0878672e1ecdc250
Jason Swails <jason.swails@gmail.com>
2018-07-18 07:04:38 -0400

    Not doing Amber for the time being.

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,23 @@ using namespace std;
 
 int main() {
 
-    ParmDataMap parmData;
-    ParmStringMap parmComments, unkParmData;
-    ParmFormatMap parmFormats;
-    string version;
-    string fname = "trx.prmtop";
-    vector<string> flagList;
+    for (int i = 0; i < 10; i++) {
+        ParmDataMap parmData;
+        ParmStringMap parmComments, unkParmData;
+        ParmFormatMap parmFormats;
+        string version;
+        string fname = "trx.prmtop";
+        vector<string> flagList;
 
-    ExitStatus retval = readparm(fname, flagList, parmData, parmComments,
-                                 unkParmData, parmFormats, version);
+        ExitStatus retval = readparm(fname, flagList, parmData, parmComments,
+                                     unkParmData, parmFormats, version);
+    }
 
+    /*
     cout << "My flags are:" << endl;
     for (ParmDataMap::const_iterator it=parmData.begin(); it!=parmData.end(); it++) {
         cout << "\t" << it->first << " size = " << (it->second).size() << endl;
     }
+    */
     return 0;
 }
