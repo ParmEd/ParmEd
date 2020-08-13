9f6ffd1609d2800563a20cd4fac4b9010c741e58
Vinicius Wilian D. Cruzeiro <vwcruzeiro@ufl.edu>
2020-08-12 17:36:58 -0700

    Add tests for the new DLPOLY conversion functionality

---
+++ test/test_parmed_dlpoly.py
@@ -0,0 +1,62 @@
+"""
+Tests the functionality in the parmed.dlpoly package
+"""
+import parmed
+from utils import (get_fn, get_saved_fn, diff_files, FileIOTestCase)
+
+class TestDlpoly(FileIOTestCase):
+    """ Tests the generation of DLPOLY input files """
+
+    def test_ala_gas(self):
+        """ Test for alanine dipeptide in gas-phase """
+        parm = get_fn('ala_gas.prmtop')
+        rst7 = get_fn('ala_gas.rst7')
+        output1 = get_fn('ala_gas.field', written=True)
+        output2 = get_fn('ala_gas.config', written=True)
+        amber = parmed.load_file(parm,rst7)
+        amber.save(output1)
+        self.assertTrue(
+            diff_files(get_saved_fn('ala_gas.field'), output1,
+                       absolute_error=1e-6)
+        )
+        amber.save(output2)
+        self.assertTrue(
+            diff_files(get_saved_fn('ala_gas.config'), output2,
+                       absolute_error=1e-6)
+        )
+
+    def test_ksi_gas(self):
+      """ Test for ksi protein in gas-phase """
+      parm = get_fn('ksi_gas.prmtop')
+      rst7 = get_fn('ksi_gas.rst7')
+      output1 = get_fn('ksi_gas.field', written=True)
+      output2 = get_fn('ksi_gas.config', written=True)
+      amber = parmed.load_file(parm,rst7)
+      amber.save(output1)
+      self.assertTrue(
+          diff_files(get_saved_fn('ksi_gas.field'), output1,
+                     absolute_error=1e-6)
+      )
+      amber.save(output2)
+      self.assertTrue(
+          diff_files(get_saved_fn('ksi_gas.config'), output2,
+                     absolute_error=1e-6)
+      )
+
+    def test_ksi_solv(self):
+      """ Test for ksi protein in solution-phase """
+      parm = get_fn('ksi_solv.prmtop')
+      rst7 = get_fn('ksi_solv.rst7')
+      output1 = get_fn('ksi_solv.field', written=True)
+      output2 = get_fn('ksi_solv.config', written=True)
+      amber = parmed.load_file(parm,rst7)
+      amber.save(output1)
+      self.assertTrue(
+          diff_files(get_saved_fn('ksi_solv.field'), output1,
+                     absolute_error=1e-6)
+      )
+      amber.save(output2)
+      self.assertTrue(
+          diff_files(get_saved_fn('ksi_solv.config'), output2,
+                     absolute_error=1e-6)
+      )
