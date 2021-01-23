290901b2b3b910fa8847399c36ffec488b0e2b4e
Jason Swails <jason.swails@gmail.com>
2021-01-23 11:23:39 -0500

    Stop trying to combine coverage for now

--- devtools/ci/install.sh
+++ devtools/ci/install.sh
@@ -8,18 +8,8 @@ conda list
 pip install -I .
 python -c "import parmed; print(parmed.__version__)"
 
-do_coverage() {
-    echo "Combining coverage data"
-    coverage combine
-    echo "Reporting..."
-    coverage report -m
-}
-
 echo "Using ParmEd version `parmed --version`"
 cd test
 ./run_scripts.sh
-# Run pytest under coverage, since that allows getting the full flexibility of
-# the coverage package without sacrificing nose functionality
 py.test --cov=parmed --durations=0 --disable-warnings .
-do_coverage
 echo "Done!"
