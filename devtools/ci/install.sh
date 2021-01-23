8ca8d09b6faa8342839a39a58983259d63a2e276
Jason Swails <jason.swails@gmail.com>
2021-01-23 01:21:04 -0500

    Use pytest instead of nose

--- devtools/ci/install.sh
+++ devtools/ci/install.sh
@@ -19,12 +19,9 @@ echo "Using ParmEd version `parmed --version`"
 cd test
 echo "Using nosetests...:"
 ./run_scripts.sh
-# Run nose under coverage, since that allows getting the full flexibility of
+# Run pytest under coverage, since that allows getting the full flexibility of
 # the coverage package without sacrificing nose functionality
 test -z "$MINIMAL_PACKAGES" && export AMBERHOME=$HOME/miniconda/envs/myenv
-coverage run --source=parmed --parallel-mode -m \
-    nose -vs --with-timer --timer-ok=5s --timer-warning=12s \
-         --timer-filter=warning,error .
-fi
+coverage run --source=parmed --parallel-mode -m pytest --durations=0 test
 do_coverage
 echo "Done!"
