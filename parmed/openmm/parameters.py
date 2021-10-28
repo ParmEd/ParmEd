f4952afb40e28430c7aec973ce2e5fca672844d7
Ivy Zhang <ivy.zhang@wustl.edu>
2021-10-28 18:46:21 -0400

    minor fix for CG in OLP

--- parmed/openmm/parameters.py
+++ parmed/openmm/parameters.py
@@ -657,7 +657,10 @@ class OpenMMParameterSet(ParameterSet, CharmmImproperMatchingMixin, metaclass=Fi
 
                 # Add extra bonds if the atom is the C in a carbonyl
                 if (bond.atom1.name == 'OD1' and bond.atom2 == atom) or (bond.atom1 == atom and bond.atom2.name == 'OD1'):
-                    bonds += 1
+                    if residue_name in ['OLP', 'NOLP', 'COLP']:
+                        pass
+                    else:
+                        bonds += 1
                 elif (bond.atom1.name == 'O' and bond.atom2 == atom) or (bond.atom1 == atom and bond.atom2.name == 'O'):
                     if residue_name == 'OME': # The O in OME is not a carbonyl
                         pass
@@ -675,6 +678,8 @@ class OpenMMParameterSet(ParameterSet, CharmmImproperMatchingMixin, metaclass=Fi
                 element_name = atom.element_name
 
             # If the number of bonds within the residue does not equal the number of bonds the atom should have, the atom  has an external bond 
+            if residue_name == 'OLP':
+                print(f"{atom.name} {d[element_name]} {bonds}")
             if d[element_name] > bonds:
                 external_bonds.append(atom)
 
