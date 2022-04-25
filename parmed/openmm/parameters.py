fd2b568f4c13d5e535c7ba82def370d71f5335c1
Ivy Zhang <ivy.zhang@wustl.edu>
2022-04-25 10:24:46 -0400

    update comment

--- parmed/openmm/parameters.py
+++ parmed/openmm/parameters.py
@@ -677,7 +677,7 @@ class OpenMMParameterSet(ParameterSet, CharmmImproperMatchingMixin, metaclass=Fi
             else:
                 element_name = atom.element_name
 
-            # If the number of bonds within the residue does not equal the number of bonds the atom should have, the atom  has an external bond 
+            # If the number of bonds within the residue is greater than the number of bonds the atom should have, the atom  has an external bond 
             if d[element_name] > bonds:
                 external_bonds.append(atom)
 
@@ -726,6 +726,7 @@ class OpenMMParameterSet(ParameterSet, CharmmImproperMatchingMixin, metaclass=Fi
                     etree.SubElement(xml_residue, 'ExternalBond', atomName=residue.tail.name)
             else:
                 external_bonds = self._get_atoms_with_external_bonds(name)
+                external_bonds = [external_bonds[0], external_bonds[-1]] + external_bonds[1:-1] if len(external_bonds) > 2 and len(external_bonds[-1].name) <= 2 else external_bonds # Hack to get rid of atom ordering diffs
                 for atom in external_bonds:
                     etree.SubElement(xml_residue, 'ExternalBond', atomName=atom.name)
             if residue.name in valid_patches_for_residue:
